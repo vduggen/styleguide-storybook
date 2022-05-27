d7d811615110a00de6895cd7380681961a53304d
ederusena <eder.sena@live.com>
2022-05-27 11:29:40 -0300

    :tada: buildando

--- storybook-vue3-tailwind/vite.config.js
+++ storybook-vue3-tailwind/vite.config.js
@@ -1,10 +1,29 @@
 import { fileURLToPath, URL } from 'url'
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/index.js'),
+      name: 'sb-unimestre-components',
+      fileName: (format) => `sb-unimestre-components.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
